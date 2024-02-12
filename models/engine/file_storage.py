a70610394397e0740221c044fdfbf405bd48ef17
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-02-12 02:37:21 -0800

    🔧

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@
 from json import dump, load
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
 from models.state import State
 from models.city import City
 from models.review import Review
